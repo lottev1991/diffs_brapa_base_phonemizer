1f48b3735fe4a98642c4b6b98c1fe30ffc99790f
HAI-D <68165064+overdramatic@users.noreply.github.com>
2025-01-25 07:36:14 -0300

    Add files via upload

---
+++ ds_brapa_base_phonemizer/DiffSingerBRAPABasePhonemizer.cs
@@ -0,0 +1,11 @@
+﻿using OpenUtau.Api;
+
+namespace OpenUtau.Core.DiffSinger
+{
+    [Phonemizer("DiffSinger BRAPA Base Phonemizer", "DIFFS BRAPA-B", language: "PT")]
+    public class DiffSingerBRAPABasePhonemizer : DiffSingerBasePhonemizer
+    {
+        protected override string GetDictionaryName()=>"dsdict-brapa.yaml";
+        protected override string GetLangCode()=>"pt";
+    }
+}
